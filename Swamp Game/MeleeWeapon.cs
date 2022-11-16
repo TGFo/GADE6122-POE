829774a788cbeee9037bdf6a7026b77b3945fda9
Tyrique Forbes <tyriqueforbes@gmail.com>
2022-11-17 01:48:20 +0200

    added Weapon absract class added MeleeWeapon subclass

---
+++ Swamp Game/MeleeWeapon.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Swamp_Game
+{
+    internal class MeleeWeapon : Weapon
+    {
+        public enum Types
+        {
+            Dagger,
+            Longsword
+        }
+        public MeleeWeapon(int Y, int X, Types meleeWeapon) : base(Y, X)
+        {
+            switch (meleeWeapon)
+            {
+                case 0:
+                    weaponType = "Dagger";
+                    durability = 10;
+                    damage = 3;
+                    cost = 3;
+                    break;
+                case (Types)1:
+                    weaponType = "Longsword";
+                    durability = 6;
+                    damage = 4;
+                    cost = 5;
+                    break;
+            }
+        }
+        public override int getRange()
+        {
+            return 1;
+        }
+
+        public override string ToString()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Swamp Game/Weapon.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Swamp_Game
+{
+    abstract class Weapon : Item
+    {
+        protected double damage;
+        protected int range;
+        protected int durability;
+        protected int cost;
+        protected string weaponType;
+        protected Weapon(int Y, int X) : base(Y, X, TileType.Weapon)
+        {
+        }
+        public double getWeaponDamage() 
+        { return damage; }
+        public virtual int getRange() 
+        { return range; }
+        public int getDurability() 
+        { return durability; }
+        public int getCost() 
+        { return cost; }
+        public void setWeaponDamage(double damage) 
+        { this.damage = damage; }
+        public virtual void setRange(int range) 
+        { this.range = range; }
+        public void setDurability(int durability)
+        { this.durability = durability; }
+        public void setCost(int cost) 
+        { this.cost = cost; }
+
+    }
+}
