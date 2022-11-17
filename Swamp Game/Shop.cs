613c5fb84db3ce26dd346a4b6787663f81d109f6
My-Normal-name <Corbinalexk@gmail.com>
2022-11-17 21:42:44 +0200

    Added and filled shop class

---
+++ Swamp Game/Shop.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public class Class1
+{
+	private new int[] Weapons = new int[3];
+	private random rand;
+	private character buyer;
+
+	public Class1()
+	{
+        
+
+
+    }
+    RandomWeapon()
+
+
+    Private Weapon RandomWeapon()
+
+	Public bool CanBuy(int num)
+
+
+    Public void Buy(int num)
+
+    Public string DisplayWeapon(int num)
+
+}
